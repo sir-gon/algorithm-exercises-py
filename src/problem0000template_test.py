b530862fa4f806d1f774b32c418d83d765027f84
Gonzalo Diaz <devel@gon.cl>
2022-09-09 23:31:19 -0400

    Problem template: Comments fixed.

--- src/problem0000template_test.py
+++ src/problem0000template_test.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python3
-
 ###############################################################################
-# Multiples of 3 and 5
+# <TITLE>
 #
-# https://projecteuler.net/problem=1
+# https://projecteuler.net/problem=XX
 #
-# If we list all the natural numbers below 10 that are multiples
-# of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# <DESCRIPTION>
 #
-# Find the sum of all the multiples of 3 or 5 below 1000.
-###############################################################################
+################################################################################
 
 import unittest
 from .problem0000template import problem0000
