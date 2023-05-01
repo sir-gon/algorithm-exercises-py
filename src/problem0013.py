673fac09e073cc8e64dfa68b84f3da621a5fc7fd
Gonzalo Diaz <devel@gon.cl>
2023-05-01 01:16:09 -0400

    Problem0013: lint problems fixed.

--- src/problem0013.py
+++ src/problem0013.py
@@ -15,11 +15,11 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 
-def problem0013(_arrayOfNumbers, _firtsDigits):
+def problem0013(_array_of_numbers, _firts_digits):
 
-    sumOfBigNumbers = sum(_arrayOfNumbers)
-    sumOfBigNumbers = str(sumOfBigNumbers)[0:_firtsDigits]
-    sumOfBigNumbers = int(sumOfBigNumbers)
+    sum_of_big_numbers = sum(_array_of_numbers)
+    sum_of_big_numbers = str(sum_of_big_numbers)[0:_firts_digits]
+    sum_of_big_numbers = int(sum_of_big_numbers)
 
-    LOGGER.info('Problem 0013 result: %i', sumOfBigNumbers)
-    return sumOfBigNumbers
+    LOGGER.info('Problem 0013 result: %i', sum_of_big_numbers)
+    return sum_of_big_numbers
--- src/problem0013_test.py
+++ src/problem0013_test.py
@@ -20,8 +20,8 @@ class TestProblem0013(unittest.TestCase):
     def test_problem0013(self):
 
         answer = 5537376230
-        firtsDigits = 10
+        firts_digits = 10
 
-        self.assertEqual( problem0013(problem0013Data, firtsDigits), answer,
+        self.assertEqual( problem0013(problem0013Data, firts_digits), answer,
             f"problem0013({problem0013Data}) must be "\
             f"=> {answer}")
