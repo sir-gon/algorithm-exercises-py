d958d83cf28ef1dac84e31101d0c354197ee692d
Gonzalo Diaz <devel@gon.cl>
2023-06-19 16:22:35 -0400

    [BUGFIX] Helper: divisors (natural_number) now produce unique values. New edge cases.

--- src/helpers/natural_number_test.py
+++ src/helpers/natural_number_test.py
@@ -63,6 +63,29 @@ class TestDivisors(unittest.TestCase):
                 f"{_} | NaturalNumber({_tt['input']}).get_divisors_cycles() "
                 f"must be => {_tt['cycles']}")
 
+    def test_divisors_edge_cases(self):
+
+        tests = [
+          {'input': 110, 'answer': [1, 2, 5, 10, 11, 22, 55, 110], 'cycles': 10},
+          {'input': 18632, 'answer': [
+              1, 2, 4, 8, 17, 34, 68, 136, 137,
+              274, 548, 1096, 2329, 4658, 9316, 18632],
+              'cycles': 136}
+        ]
+
+        for _, _tt in enumerate(tests):
+            to_test = NaturalNumber(_tt['input'])
+
+            self.assertEqual(
+                to_test.divisors(), _tt['answer'],
+                f"{_} | NaturalNumber({_tt['input']}).divisors() must be "
+                f"=> {_tt['answer']}")
+
+            self.assertEqual(
+                to_test.get_divisors_cycles(), _tt['cycles'],
+                f"{_} | NaturalNumber({_tt['input']}).get_divisors_cycles() "
+                f"must be => {_tt['cycles']}")
+
     def test_prime_factor(self):
         tests = [
           {'input': 1, 'answer': 1, 'divisor': 1, 'cycles': 0},
