57fefb89291646550372bc25c1ceeca084d1cf93
Gonzalo Diaz <devel@gon.cl>
2023-06-26 01:06:34 -0400

    Problem 0020: solved

---
+++ src/problem0020_test.py
@@ -0,0 +1,36 @@
+###############################################################################
+# <TITLE>
+#
+# https://projecteuler.net/problem=XX
+#
+# <DESCRIPTION>
+#
+################################################################################
+
+import unittest
+from .problem0020 import problem0020
+
+
+class TestProblem0020(unittest.TestCase):
+
+    def test_problem0020_small(self):
+
+        input_factorial = 10
+        solution_found = 27
+
+        self.assertEqual(
+            problem0020(input_factorial),
+            solution_found,
+            f"problem0020({input_factorial}) must be "
+            f"=> {solution_found}")
+
+    def test_problem0020_full(self):
+
+        input_factorial = 100
+        solution_found = 648
+
+        self.assertEqual(
+            problem0020(input_factorial),
+            solution_found,
+            f"problem0020({input_factorial}) must be "
+            f"=> {solution_found}")
