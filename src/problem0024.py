e436fbb0658c99cc4031abab3eeed24b8c24afbe
Gonzalo Diaz <devel@gon.cl>
2023-07-10 20:08:20 -0400

    [REFACTOR] Problem 0024: better algorithm to solve.

--- src/problem0024.py
+++ src/problem0024.py
@@ -16,79 +16,35 @@
 ################################################################################
 
 import logging
-from typing import List
+import math
 
 LOGGER = logging.getLogger(__name__)
 
 
-class Permutations:
-    # pylint: disable=too-few-public-methods
+def permute(symbols: str, target: int):
+    choices = list(symbols)
+    answer = ""
+    minimum = 0
 
-    # "inner global variables" to catch the results shared across recursive branch calls.
-    current_cycle = 0
-    last_branch_collector: List[str] = []
+    while len(choices) > 0:
+        index = 0
+        combos = math.factorial(len(choices)-1)
+        minimum += combos
+        while target > minimum:
+            index += 1
+            minimum += combos
+        answer += choices.pop(index)
+        minimum -= combos
 
-    def lexicographic_permutation_find(
-        self, elements: List[str], number_of_permutation_to_find: int
-    ) -> List[str]:
-
-        # Initial values
-        self.current_cycle = 0
-        self.last_branch_collector: List[str] = []
-        init_branch_collector: List[str] = []
-
-        def compute_permutations(
-            stop_at_cycle: int, input_elements: List[str], branch_collector: List[str]
-        ) -> None:
-            if self.current_cycle >= stop_at_cycle:
-                return
-
-            for i in input_elements:
-                root_element = i
-                rest_of_elements = []
-
-                for j in input_elements:
-                    if i != j:
-                        rest_of_elements.append(j)
-
-                new_branch_collector = branch_collector.copy()
-                new_branch_collector.append(root_element)
-
-                # finally
-                if len(rest_of_elements) > 0:
-                    LOGGER.debug('REST: %s', rest_of_elements)
-
-                    compute_permutations(
-                        stop_at_cycle,
-                        rest_of_elements,
-                        new_branch_collector
-                    )
-                else:
-                    self.last_branch_collector = new_branch_collector
-                    self.current_cycle += 1
-
-                    LOGGER.debug('FINISH BRANCH: %i -> %s',
-                                 self.current_cycle,
-                                 rest_of_elements)
-
-            return
-
-        compute_permutations(
-            number_of_permutation_to_find,
-            elements,
-            init_branch_collector)
-
-        return self.last_branch_collector
+    return answer
 
 
 def problem0024(
-    input_elements: List[str],
+    input_elements: str,
     input_permutation_to_find: int
-) -> List[str]:
-    permutation_found = Permutations().lexicographic_permutation_find(
-        input_elements,
-        input_permutation_to_find
-    )
+) -> str:
+
+    permutation_found = permute(input_elements, input_permutation_to_find)
 
     LOGGER.info("Problem 0024 result: %s", permutation_found)
     return permutation_found
