7550510c7dbc79366d829fc85c054c8b3cc902f3
Gonzalo Diaz <devel@gon.cl>
2023-07-27 20:47:06 -0400

    [Hacker Rank]: Solve Me First solved ✓

---
+++ src/hackerrank/solve_me_first_test.py
@@ -0,0 +1,18 @@
+import unittest
+from .solve_me_first import solve_me_first
+
+
+class TestSolveMeFirst(unittest.TestCase):
+
+    def test_solve_me_first(self):
+
+        tests = [
+          {'a': 0, 'b': 0, 'answer': 0}
+        ]
+
+        for _, _tt in enumerate(tests):
+
+            self.assertEqual(
+                solve_me_first(_tt['a'], _tt['b']), _tt['answer'],
+                f"{_} | solve_me_first({_tt['a'], _tt['b']}) must be "
+                f"=> {_tt['answer']}")
