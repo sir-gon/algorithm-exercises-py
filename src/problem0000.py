668717160a78b1241ecdd314e10dbd13b7b929c6
Gonzalo Diaz <devel@gon.cl>
2023-07-27 21:47:49 -0400

    [REFACTOR] type hinting added to template problem.

--- src/problem0000.py
+++ src/problem0000.py
@@ -3,7 +3,7 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 
-def problem0000(_input):
+def problem0000(_input: int) -> int:
     result = _input
 
     LOGGER.info('Problem 0000 result: %i', result)
