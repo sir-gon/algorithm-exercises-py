bf87a5b11885c243b2d168acf5b4fe8d2efaa47e
Gonzalo Diaz <devel@gon.cl>
2023-07-29 16:45:54 -0400

    [Hacker Rank]: Diagonal Difference solved ✓

---
+++ src/hackerrank/diagonal_difference.py
@@ -0,0 +1,20 @@
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def diagonal_difference(arr: list[list[int]]) -> int:
+    diag1 = 0
+    diag2 = 0
+    last = len(arr) - 1
+
+    for i, line in enumerate(arr):
+        for j, cell in enumerate(line):
+            if i == j:
+                diag1 += cell
+                diag2 += arr[last - i][j]
+
+    result = abs(diag1 - diag2)
+
+    LOGGER.info('Diagonal Difference result: %i', result)
+    return result
