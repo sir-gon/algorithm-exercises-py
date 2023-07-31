db510f746e98136d509840288c02eecaaaab7c58
Gonzalo Diaz <devel@gon.cl>
2023-07-30 21:21:22 -0400

    [Hacker Rank]: Between Two Sets solved ✓

---
+++ src/hackerrank/implementation/between_two_sets.py
@@ -0,0 +1,53 @@
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def is_factor(_n: int, group: list[int]) -> bool:
+    result: bool = True
+    i: int = 0
+
+    if len(group) == 0:
+        return False
+
+    while (i < len(group) and result):
+        if _n % group[i] != 0:
+            result = False
+
+        i += 1
+
+    return result
+
+
+def factor_of(_n: int, group: list[int]):
+    result: bool = True
+    i: int = 0
+
+    if len(group) == 0:
+        return False
+
+    while (i < len(group) and result):
+        if group[i] % _n != 0:
+            result = False
+
+        i += 1
+
+    return result
+
+
+def get_total_x(_a: list[int], _b: list[int]) -> int:
+    _max_ = 0
+    for _, j in enumerate(_b):
+        if j > _max_:
+            _max_ = j
+
+    result: list[int] = []
+
+    for i in range(2, _max_):
+        if is_factor(i, _a) and factor_of(i, _b):
+            result.append(i)
+
+    output = len(result)
+    LOGGER.info('Between Two Sets result: %i', output)
+
+    return output
