82b9c8129a73185dc1df98bb90df769c0fb94998
Gonzalo Diaz <devel@gon.cl>
2023-08-01 18:24:45 -0400

    [REFACTOR] sonarlint fixes

--- src/projecteuler/helpers/matrix.py
+++ src/projecteuler/helpers/matrix.py
@@ -5,6 +5,6 @@ from typing import List
 def matrix(t_n: int, t_m: int, init: int | None) -> List[List[int]]:
     result = []
 
-    for i in range(0, t_n):
-        result.append([init for i in range(t_m)])
+    for _ in range(0, t_n):
+        result.append([init for _ in range(t_m)])
     return result
