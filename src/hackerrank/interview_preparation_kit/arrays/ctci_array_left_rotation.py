3a9056276cf6c79da6de3e9840b155bed8837ed6
Gonzalo Diaz <devel@gon.cl>
2024-02-18 00:34:51 -0300

    [Hacker Rank] Interview Preparation Kit: Arrays: Left Rotation. First solution.

--- src/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation.py
+++ src/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation.py
@@ -1,10 +1,25 @@
 # @link Problem definition
-# [[docs/hackerrank/interview-preparation-kit/arrays/ctci_array_left_rotation.md]]
+# [[docs/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation.md]]
 
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
 
-def rot_left(group: list[int]) -> list[int]:
-    return group
+def rot_left_one(group: list[int]) -> list[int]:
+
+    result = group[1:len(group)]
+    result = result + [group[0]]
+
+    return result
+
+
+def rot_left(group: list[int], d: int) -> list[int]:
+
+    output = group.copy()
+    i = 1
+    while i <= d:
+        output = rot_left_one(output)
+        i += 1
+
+    return output
--- src/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation_test.py
+++ src/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation_test.py
@@ -1,18 +1,35 @@
 import unittest
-from .ctci_array_left_rotation import rot_left
+from .ctci_array_left_rotation import rot_left, rot_left_one
 
 
 class TestArrayLeftRotation(unittest.TestCase):
 
-    def test_ctci_array_left_rotation(self):
+    def test_rot_left_one(self):
 
         tests = [
-          {'input': [1, 2, 3, 4, 5], 'answer': [1, 2, 3, 4, 5]},
+          {'input': [1, 2, 3, 4, 5], 'answer': [2, 3, 4, 5, 1]},
+          {'input': [2, 3, 4, 5, 1], 'answer': [3, 4, 5, 1, 2]},
+          {'input': [3, 4, 5, 1, 2], 'answer': [4, 5, 1, 2, 3]},
+          {'input': [4, 5, 1, 2, 3], 'answer': [5, 1, 2, 3, 4]},
+          {'input': [5, 1, 2, 3, 4], 'answer': [1, 2, 3, 4, 5]},
         ]
 
         for _, _tt in enumerate(tests):
 
             self.assertEqual(
-                rot_left(_tt['input']), _tt['answer'],
+                rot_left_one(_tt['input']), _tt['answer'],
+                f"{_} | rot_left({_tt['input']}) must be "
+                f"=> {_tt['answer']}")
+
+    def test_rot_left(self):
+
+        tests = [
+          {'input': [1, 2, 3, 4, 5], 'd': 4, 'answer': [5, 1, 2, 3, 4]},
+        ]
+
+        for _, _tt in enumerate(tests):
+
+            self.assertEqual(
+                rot_left(_tt['input'], _tt['d']), _tt['answer'],
                 f"{_} | rot_left({_tt['input']}) must be "
                 f"=> {_tt['answer']}")
