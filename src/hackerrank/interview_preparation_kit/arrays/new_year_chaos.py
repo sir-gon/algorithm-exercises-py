3bb078f25ab6715dd72f2e8f10ff15474ab533b7
Gonzalo Diaz <devel@gon.cl>
2024-02-19 13:20:46 -0300

    [Hacker Rank] Interview Preparation Kit: Arrays: New Year Chaos. Solved ✓.

--- src/hackerrank/interview_preparation_kit/arrays/new_year_chaos.py
+++ src/hackerrank/interview_preparation_kit/arrays/new_year_chaos.py
@@ -5,6 +5,29 @@ import logging
 
 LOGGER = logging.getLogger(__name__)
 
+TOO_CHAOTIC_ERROR = 'Too chaotic'
+
 
 def minimum_bribes(q: list[int]) -> 'int | None':
-    return len(q)
+    bribes = 0
+
+    for i, o in enumerate(q):
+        cur = i + 1
+
+        if o - cur > 2:
+            return None
+
+        for k in q[max(o - 2, 0):i]:
+            if k > o:
+                bribes += 1
+
+    return bribes
+
+
+def minimum_bribes_transform(q: list[int]) -> 'int | str':
+
+    result = minimum_bribes(q)
+    if result is None:
+        return TOO_CHAOTIC_ERROR
+
+    return result
