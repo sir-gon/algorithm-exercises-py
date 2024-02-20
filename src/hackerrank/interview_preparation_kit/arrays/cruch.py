2ad0601f78283cadae014e91dc408f2935bcbbbe
Gonzalo Diaz <devel@gon.cl>
2024-02-20 15:44:58 -0300

    [Hacker Rank] Interview Preparation Kit: Arrays: Array Manipulation. Partially Solved ✓. Little bit refactor. Still have some test cases producing "Time limit exceeded".

--- src/hackerrank/interview_preparation_kit/arrays/cruch.py
+++ src/hackerrank/interview_preparation_kit/arrays/cruch.py
@@ -8,22 +8,18 @@ LOGGER = logging.getLogger(__name__)
 
 def array_manipulation(n: int, queries: list[list[int]]) -> int:
 
-    result = [0] * n
+    result = [0] * (n + 1)
+    maximum = 0
 
-    for row in queries:
-
-        start = row[0] - 1
-        end = row[1]
-        k = row[2]
+    for [a, b, k] in queries:
 
         LOGGER.debug("start -> %s", result)
-        for j in range(start, end):
+        for j in range(a, b+1):
             result[j] += k
             LOGGER.debug("result -> %s", result)
 
-    maximum = None
     for value in result:
-        if maximum is None or value > maximum:
+        if value > maximum:
             maximum = value
 
-    return maximum or 0
+    return maximum
