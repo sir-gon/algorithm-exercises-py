478a4d83012d4a2077015076a990d6326507dd25
Gonzalo Diaz <devel@gon.cl>
2024-02-20 16:35:22 -0300

    [Hacker Rank] Interview Preparation Kit: Arrays: Array Manipulation. Solved ✓.

---
+++ src/hackerrank/interview_preparation_kit/arrays/cruch_optimized.py
@@ -0,0 +1,29 @@
+# @link Problem definition
+# [[docs/hackerrank/interview_preparation_kit/arrays/crush.md]]
+
+# @see Solution Notes:
+# [[docs/hackerrank/interview_preparation_kit/arrays/crush_optimized-solution-notes.md]]
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def array_manipulation_optimized(n: int, queries: list[list[int]]) -> int:
+    # why adding 2?
+    #   first slot to adjust 1-based index and
+    #   last slot for storing accum_sum result
+    result = [0] * (n + 2)
+    maximum = 0
+
+    for [a, b, k] in queries:
+        # Prefix
+        result[a] += k
+        result[b+1] -= k
+
+    accum_sum = 0
+    for value in result:
+        accum_sum += value
+        maximum = max(maximum, accum_sum)
+
+    return maximum
