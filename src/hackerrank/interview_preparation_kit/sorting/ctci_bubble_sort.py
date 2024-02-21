499eb00310028566e5bf1dff44ab1eba14d79da0
Gonzalo Diaz <devel@gon.cl>
2024-02-21 15:25:42 -0300

    [Hacker Rank] Interview Preparation Kit: Sorting: Bubble Sort. Solved ✓.

---
+++ src/hackerrank/interview_preparation_kit/sorting/ctci_bubble_sort.py
@@ -0,0 +1,54 @@
+# @link Problem definition
+# [[docs/hackerrank/interview_preparation_kit/sort/ctci-bubble-sort.md]]
+
+from __future__ import annotations
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+# pylint: disable=R0903:too-few-public-methods
+class SortableGroup:
+    group: list[int]
+    count: int
+
+    def __init__(self, group: list[int]):
+        self.count = 0
+        self.group = group
+
+    def bubble_sort(self) -> SortableGroup:
+
+        group = self.group[:]  # copy original list
+
+        n = len(group)
+        count = 0
+        i = 0
+
+        while i < n:
+
+            for j in range(0, len(group) - 1):
+
+                if group[j] > group[j + 1]:
+                    temp = group[j]
+                    group[j] = group[j + 1]
+                    group[j + 1] = temp
+                    count += 1
+            i += 1
+
+        self.count = count
+        self.group = group
+
+        return self
+
+
+def count_swaps(group: list[int]) -> str:
+
+    sortable_group = SortableGroup(group)
+    sortable_group.bubble_sort()
+
+    nl = "\n"
+    output = f'Array is sorted in {sortable_group.count} swaps.{nl}' + \
+             f'First Element: {sortable_group.group[0]}{nl}' + \
+             f'Last Element: {sortable_group.group[len(sortable_group.group)-1]}{nl}'
+
+    return output
