221ad71b05ad10f949f4a1468d6e87e7cffb3e51
Gonzalo Diaz <devel@gon.cl>
2024-02-21 16:33:16 -0300

    [RAFACTOR] [Hacker Rank] Interview Preparation Kit: Sorting: Bubble Sort. Test rewrite.

--- src/hackerrank/interview_preparation_kit/sorting/ctci_bubble_sort_test.py
+++ src/hackerrank/interview_preparation_kit/sorting/ctci_bubble_sort_test.py
@@ -9,54 +9,50 @@ ANSWER_TEMPLATE = f'Array is sorted in {NUM_SWAPS} swaps.{NL}' + \
                   f'First Element: {FIRST_ELEMENT}{NL}' + \
                   f'Last Element: {LAST_ELEMENT}{NL}'
 
+TEST_CASES = [
+                {
+                    'input': [6, 4, 1],
+                    'sorted': [1, 4, 6],
+                    'answer': ANSWER_TEMPLATE[:]
+                    .replace(NUM_SWAPS, '3')
+                    .replace(FIRST_ELEMENT, '1')
+                    .replace(LAST_ELEMENT, '6')
+                },
+                {
+                    'input': [3, 2, 1],
+                    'sorted': [1, 2, 3],
+                    'answer': ANSWER_TEMPLATE[:]
+                    .replace(NUM_SWAPS, '3')
+                    .replace(FIRST_ELEMENT, '1')
+                    .replace(LAST_ELEMENT, '3')
+                },
+                {
+                    'input': [1, 2, 3],
+                    'sorted': [1, 2, 3],
+                    'answer': ANSWER_TEMPLATE[:]
+                    .replace(NUM_SWAPS, '0')
+                    .replace(FIRST_ELEMENT, '1')
+                    .replace(LAST_ELEMENT, '3')
+                }
+            ]
+
 
 class TestBubleSort(unittest.TestCase):
 
     def test_sorts(self):
 
-        tests = [
-            {'input': SortableGroup([6, 4, 1]), 'answer': [1, 4, 6]},
-            {'input': SortableGroup([3, 2, 1]), 'answer': [1, 2, 3]},
-            {'input': SortableGroup([1, 2, 3]), 'answer': [1, 2, 3]}
-        ]
+        for _, _tt in enumerate(TEST_CASES):
 
-        for _, _tt in enumerate(tests):
+            to_test = SortableGroup(_tt['input'])
 
             self.assertEqual(
-                _tt['input'].bubble_sort().group, _tt['answer'],
-                f"{_} | count_swaps({_tt['input']}) must be "
-                f"=> {_tt['answer']}")
+                to_test.bubble_sort().group, _tt['sorted'],
+                f"{_} | SortableGroup({_tt['input']}).bubble_sort().group must be "
+                f"=> {_tt['sorted']}")
 
     def test_counts(self):
 
-        tests = [
-            {
-                'input': [6, 4, 1],
-                'answer':
-                    ANSWER_TEMPLATE[:]
-                    .replace(NUM_SWAPS, '3')
-                    .replace(FIRST_ELEMENT, '1')
-                    .replace(LAST_ELEMENT, '6')
-            },
-            {
-                'input': [3, 2, 1],
-                'answer':
-                    ANSWER_TEMPLATE[:]
-                    .replace(NUM_SWAPS, '3')
-                    .replace(FIRST_ELEMENT, '1')
-                    .replace(LAST_ELEMENT, '3')
-            },
-            {
-                'input': [1, 2, 3],
-                'answer':
-                    ANSWER_TEMPLATE[:]
-                    .replace(NUM_SWAPS, '0')
-                    .replace(FIRST_ELEMENT, '1')
-                    .replace(LAST_ELEMENT, '3')
-            },
-        ]
-
-        for _, _tt in enumerate(tests):
+        for _, _tt in enumerate(TEST_CASES):
 
             self.assertEqual(
                 count_swaps(_tt['input']), _tt['answer'],
