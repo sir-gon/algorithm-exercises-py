fac8c2dc4bae7633be4ebc27023c77874e50c1b8
Gonzalo Diaz <devel@gon.cl>
2024-03-06 00:55:17 -0300

    [Hacker Rank] Interview Preparation Kit: Linked Lists. Inserting a Node Into a Sorted Doubly Linked List. Solved ✓.

---
+++ src/hackerrank/interview_preparation_kit/linked_lists/insert_a_node_into_a_sorted_doubly_linked_list_test.py
@@ -0,0 +1,77 @@
+import unittest
+
+from .lib.doubly_linked_list import DoublyLinkedList, double_linked_list_to_text
+from .insert_a_node_into_a_sorted_doubly_linked_list import sorted_insert
+
+
+TEST_CASES = [
+    {
+        'title': 'Sample Test case 0',
+        'llist': DoublyLinkedList()
+        .insert_node(1)
+        .insert_node(2)
+        .insert_node(4),
+        'data': 3,
+        'answer': '1 2 3 4'
+    },
+    {
+        'title': 'Sample Test case 1',
+        'llist': DoublyLinkedList()
+        .insert_node(2)
+        .insert_node(3)
+        .insert_node(4),
+        'data': 1,
+        'answer': '1 2 3 4'
+    },
+    {
+        'title': 'Sample Test case 2',
+        'llist': DoublyLinkedList()
+        .insert_node(1)
+        .insert_node(2)
+        .insert_node(3),
+        'data': 4,
+        'answer': '1 2 3 4'
+    }
+]
+
+
+class TestMakeAnagramt(unittest.TestCase):
+
+    def test_sorted_insert(self):
+
+        for _, _tt in enumerate(TEST_CASES):
+
+            llist = sorted_insert(
+                    _tt['llist'].head,
+                    _tt['data'])
+
+            result = double_linked_list_to_text(llist, ' ')
+
+            self.assertEqual(
+                result,
+                _tt['answer'],
+                f"{_} | sorted_insert("
+                f"{_tt['llist'].head},"
+                f"{_tt['data']},"
+                f") must be => {_tt['answer']}")
+
+    def test_sorted_insert_edge_case(self):
+
+        llist = DoublyLinkedList().insert_node(2)
+        data = 1
+
+        head = sorted_insert(
+                    llist.head,
+                    data)
+
+        result = double_linked_list_to_text(head, ' ')
+
+        answer = '1 2'
+
+        self.assertEqual(
+            result,
+            answer,
+            f"sorted_insert("
+            f"{llist},"
+            f"{data}"
+            f") must be => {answer}")
---
+++ src/hackerrank/interview_preparation_kit/linked_lists/lib/doubly_linked_list.py
@@ -0,0 +1,46 @@
+# Given by problem:
+# https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list
+
+# pylint: disable=too-few-public-methods,duplicate-code
+# pyright: reportOptionalMemberAccess=none
+
+from __future__ import annotations
+
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data: int = node_data
+        self.next: DoublyLinkedListNode | None = None
+        self.prev: DoublyLinkedListNode | None = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+        self.tail = node
+        return self
+
+
+def double_linked_list_to_text(node, sep):
+    output: str = ''
+
+    while node:
+        output = output + str(node.data)
+
+        node = node.next
+
+        if node:
+            output = output + sep
+
+    return output
