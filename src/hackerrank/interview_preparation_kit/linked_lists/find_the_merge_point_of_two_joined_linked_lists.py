cf8197e2b666118779beecc2678970c9317d367c
Gonzalo Diaz <devel@gon.cl>
2024-03-08 15:23:54 -0300

    [Hacker Rank] Interview Preparation Kit: Linked Lists: Find Merge Point of Two Lists. Solved ✓.

---
+++ src/hackerrank/interview_preparation_kit/linked_lists/find_the_merge_point_of_two_joined_linked_lists.py
@@ -0,0 +1,30 @@
+# pylint: disable=line-too-long
+# @link Problem definition [[docs/hackerrank/interview_preparation_kit/linked_lists/find-the-merge-point-of-two-joined-linked-lists.md]] # noqa: E501
+# pylint: enable=line-too-long
+
+from .lib.singly_linked_list import SinglyLinkedListNode
+
+
+def find_merge_node(
+        head1: SinglyLinkedListNode,
+        head2: SinglyLinkedListNode) -> int | None:
+    llindex: list[SinglyLinkedListNode] = []
+
+    result: int | None = None
+    node = head1
+
+    while node:
+        llindex.append(node)
+        node = node.next
+
+    node = head2
+    resume = True
+    while node and resume:
+        if node in llindex:
+            result = node.data
+            resume = False
+
+        llindex.append(node)
+        node = node.next
+
+    return result
