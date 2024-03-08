904c5ba7ae94aaf81540e1e46f6b5d30a4229fdc
Gonzalo Diaz <devel@gon.cl>
2024-03-08 19:15:46 -0300

    [REFACTOR] some Sonarlint Code Smell fixed

--- src/projecteuler/helpers/palindrome_test.py
+++ src/projecteuler/helpers/palindrome_test.py
@@ -6,14 +6,14 @@ class TestPalindromes(unittest.TestCase):
 
     def test_is_palindrome(self):
 
-        self.assertEqual(is_palindrome(0), True)
-        self.assertEqual(is_palindrome(7), True)
-        self.assertEqual(is_palindrome(101), True)
-        self.assertEqual(is_palindrome(9889), True)
+        self.assertTrue(is_palindrome(0))
+        self.assertTrue(is_palindrome(7))
+        self.assertTrue(is_palindrome(101))
+        self.assertTrue(is_palindrome(9889))
 
     def test_is_not_palindrome(self):
 
-        self.assertEqual(is_palindrome(13), False)
-        self.assertEqual(is_palindrome(29), False)
-        self.assertEqual(is_palindrome(123), False)
-        self.assertEqual(is_palindrome(534), False)
+        self.assertFalse(is_palindrome(13), False)
+        self.assertFalse(is_palindrome(29), False)
+        self.assertFalse(is_palindrome(123), False)
+        self.assertFalse(is_palindrome(534), False)
--- src/projecteuler/helpers/prime_test.py
+++ src/projecteuler/helpers/prime_test.py
@@ -6,14 +6,14 @@ class TestPrimes(unittest.TestCase):
 
     def test_(self):
 
-        self.assertEqual(is_prime(1), False)
-        self.assertEqual(is_prime(2), True)
-        self.assertEqual(is_prime(7), True)
-        self.assertEqual(is_prime(13), True)
+        self.assertFalse(is_prime(1))
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(7))
+        self.assertTrue(is_prime(13))
 
     def test_not_prime(self):
 
-        self.assertEqual(is_prime(4), False)
-        self.assertEqual(is_prime(10), False)
-        self.assertEqual(is_prime(100), False)
-        self.assertEqual(is_prime(3000), False)
+        self.assertFalse(is_prime(4))
+        self.assertFalse(is_prime(10))
+        self.assertFalse(is_prime(100))
+        self.assertFalse(is_prime(3000))
