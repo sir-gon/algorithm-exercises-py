74bb29ad310de5f57270efa20b323e93078dd259
Gonzalo Diaz <devel@gon.cl>
2024-03-24 22:52:21 -0300

    [Hacker Rank] Interview Preparation Kit: Miscellaneous: Ice Cream Parlor. New solution using binary search. It pass known test-cases but, don't pass all test-cases (wrong answer).

--- src/hackerrank/interview_preparation_kit/search/ctci_ice_cream_parlor.py
+++ src/hackerrank/interview_preparation_kit/search/ctci_ice_cream_parlor.py
@@ -1,6 +1,7 @@
 # Brute force
 
-def what_flavors(cost, money):
+def what_flavors_brute_force(cost, money):
+
     for i, x in enumerate(cost):
 
         budget = money - x
@@ -9,5 +10,40 @@ def what_flavors(cost, money):
             if budget - cost[j] == 0:
                 print(f'{i + 1} {j + 1}')
                 return [i + 1, j + 1]
+
+    return []
+
+
+# binary search
+def what_flavors(cost, money):
+
+    flavors = []
+    for index, i in enumerate(cost):
+        flavors.append({
+            'index': index,
+            'value': i
+        })
+    flavors.sort(key=lambda item: item['value'])
+
+    for i, x in enumerate(flavors):
+        budget = money - x['value']
+
+        left: int = 0
+        right: int = len(flavors) - 1
+        center: int = len(flavors) // 2
+
+        while right - left > 1:
+            if budget == flavors[center]['value']:
+                return sorted([i + 1, flavors[center]['index'] + 1])
+
+            if budget < flavors[center]['value']:
+                # left conservates
+                right = center
+
+            if budget > flavors[center]['value']:
+                left = center
+                #
+
+            center = left + (right - left) // 2
 
     return []
