c23615691183a54b929deccca0c2f2ba7503d8be
Gonzalo Diaz <devel@gon.cl>
2024-04-03 19:52:54 -0300

    [Hacker Rank] Interview Preparation Kit: Recursion and Backtracking: Recursion: Davis' Staircase. Initiate. Cache mechanism added. Solved ✓.

--- src/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_recursive_staircase.py
+++ src/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_recursive_staircase.py
@@ -2,21 +2,25 @@
 # @link Problem definition [[docs/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci-recursive-staircase.md]] # noqa
 # pylint: enable=line-too-long
 
-def step_perms_compute(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
+from typing import Dict
+
+
+def step_perms_comput_with_cache(n: int, cache: Dict[int, int]) -> int:
+    if 0 <= n <= 2:
+        return n
     if n == 3:
         return 4
 
-    return \
-        step_perms_compute(n - 3) + \
-        step_perms_compute(n - 2) + \
-        step_perms_compute(n - 1)
+    result = 0
+    for i in range(1, 4):
+        if n - i not in cache:
+            cache[n - i] = step_perms_comput_with_cache(n - i, cache)
+
+        result += cache[n - i]
+
+    return result
 
 
-def step_perms(n):
-    return step_perms_compute(n) % (10 ** 10 + 7)
+def step_perms(n: int) -> int:
+    initial_cache: Dict[int, int] = {}
+    return step_perms_comput_with_cache(n, initial_cache) % (10 ** 10 + 7)
