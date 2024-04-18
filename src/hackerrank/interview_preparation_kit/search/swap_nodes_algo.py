103e764417cee93d2a53f740078c3bfa00b0224b
Gonzalo Diaz <devel@gon.cl>
2024-04-18 14:52:23 -0400

    [Hacker Rank] Interview Preparation Kit: Search: Swap Nodes [Algo]. Initiate: Tree building. Problem definition added.

---
+++ src/hackerrank/interview_preparation_kit/search/swap_nodes_algo.py
@@ -0,0 +1,57 @@
+# pylint: disable=line-too-long
+# @link Problem definition [[docs/hackerrank/interview_preparation_kit/search/swap-nodes-algo.md]] # noqa
+# pylint: enable=line-too-long
+
+from typing import Dict, List
+from ...lib.node import Node
+
+
+def traverse_leaves(
+        root: Node | None,
+        collect: Dict[int, list[Node]],
+        level: int = 1) -> list[int] | None:
+    if root is not None:
+
+        traverse_leaves(root.left, collect, level + 1)
+
+        if root.left is None and root.right is None:
+            if level not in collect:
+                collect[level] = [root]
+            else:
+                collect[level].append(root)
+
+        traverse_leaves(root.right, collect,  level + 1)
+
+
+def build_tree(indexes: List[List[int]]) -> Node:
+    root: Node = Node(1)
+    leaves = {}
+
+    while len(indexes) > 0:
+        traverse_leaves(root, leaves)
+
+        last_level: List[Node] = list(leaves)[-1]
+
+        for leaf in leaves[last_level]:
+            if len(indexes) > 0:
+                element: List[int] = indexes.pop(0)
+                print(element)
+                if element[0] != -1:
+                    leaf.left = Node(element[0])
+                if element[1] != -1:
+                    leaf.right = Node(element[1])
+            else:
+                return root
+
+    return root
+
+
+def swap_nodes(indexes: List[List[int]], queries: List[int]) -> List[List[int]]:
+    # Write your code here
+    print(indexes)
+    print('------')
+    print(queries)
+
+    tree: Node = build_tree(indexes)
+
+    return []
---
+++ src/hackerrank/interview_preparation_kit/search/swap_nodes_algo_test.py
@@ -0,0 +1,40 @@
+import unittest
+from .swap_nodes_algo import swap_nodes
+
+
+class TestSwapNodesAlgo(unittest.TestCase):
+
+    tests = [
+        # {
+        #     'title': 'Sample Test Case 0',
+        #     'nodes': [[2, 3], [-1, -1], [-1, -1]],
+        #     'queries': [1, 1],
+        #     'answer': [[3, 1, 2], [2, 1, 3]]
+        # },
+        # {
+        #     'title': 'Sample Test Case 0',
+        #     'nodes': [[2, 3], [4, 5], [6, -1], [-1, 7], [8, 9], [10, 11], [12, 13],
+        #               [-1, 14], [-1, -1], [15, -1], [16, 17], [-1, -1], [-1, -1],
+        #               [-1, -1], [-1, -1], [-1, -1], [-1, -1]],
+        #     'queries': [2, 3],
+        #     'answer': [[14, 8, 5, 9, 2, 4, 13, 7, 12, 1, 3, 10, 15, 6, 17, 11, 16],
+        #                [9, 5, 14, 8, 2, 13, 7, 12, 4, 1, 3, 17, 11, 16, 6, 10, 15]]
+        # },
+        {
+            'title': 'Sample 2',
+            'nodes': [[2, 3], [4, -1], [5, -1], [6, -1], [7, 8], [-1, 9],
+                      [-1, -1], [10, 11], [-1, -1], [-1, -1], [-1, -1]],
+            'queries': [2, 4],
+            'answer': [[2, 9, 6, 4, 1, 3, 7, 5, 11, 8, 10],
+                       [2, 6, 9, 4, 1, 3, 7, 5, 10, 8, 11]]
+        }
+    ]
+
+    def test_swap_nodes(self):
+
+        for _, _tt in enumerate(self.tests):
+
+            self.assertEqual(
+                swap_nodes(_tt['nodes'], _tt['queries']), _tt['answer'],
+                f"{_} | swap_nodes({_tt['nodes'], _tt['queries']}) must be "
+                f"=> {_tt['answer']}")
