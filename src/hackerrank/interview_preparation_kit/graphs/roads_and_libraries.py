e5c24cfec921a64f01c6da0049cd2f92ce13a1b6
Gonzalo Diaz <devel@gon.cl>
2024-07-01 14:07:55 -0400

    [REFACTOR] clean code: variable naming

--- src/hackerrank/interview_preparation_kit/graphs/roads_and_libraries.py
+++ src/hackerrank/interview_preparation_kit/graphs/roads_and_libraries.py
@@ -7,9 +7,9 @@ class RoadsAndLibraries:
     _paths = []
     _unions = 0
 
-    def __init__(self, n: int, cities: list[list[int]]):
+    def __init__(self, q_paths: int, cities: list[list[int]]):
 
-        self._paths = [-1 for _ in range(n + 1)]
+        self._paths = [-1 for _ in range(q_paths + 1)]
 
         for path in cities:
             city_a, city_b = path[0], path[1]
@@ -19,26 +19,30 @@ class RoadsAndLibraries:
     def find(self, a: int):
         return a if self._paths[a] < 0 else self.find(self._paths[a])
 
-    def unite(self, a: int, b: int) -> bool:
-        a = self.find(a)
-        b = self.find(b)
-        if a == b:
+    def unite(self, _point_a: int, _point_b: int) -> bool:
+        point_a = self.find(_point_a)
+        point_b = self.find(_point_b)
+        if point_a == point_b:
             return False
-        if self._paths[a] > self._paths[b]:
-            a, b = b, a
-        self._paths[a] += self._paths[b]
-        self._paths[b] = a
+        if self._paths[point_a] > self._paths[point_b]:
+            point_a, point_b = point_b, point_a
+        self._paths[point_a] += self._paths[point_b]
+        self._paths[point_b] = point_a
         return True
 
     def get_unions(self) -> int:
         return self._unions
 
 
-def roads_and_libraries(n: int, c_lib: int, c_road: int, cities: list[list[int]]) -> int:
+def roads_and_libraries(
+        q_paths: int,
+        c_lib: int,
+        c_road: int,
+        cities: list[list[int]]) -> int:
 
-    ral = RoadsAndLibraries(n, cities)
+    ral = RoadsAndLibraries(q_paths, cities)
     unions = ral.get_unions()
 
-    ans1 = c_lib * n
-    ans2 = c_road * unions + c_lib * (n - unions)
+    ans1 = c_lib * q_paths
+    ans2 = c_road * unions + c_lib * (q_paths - unions)
     return min(ans1, ans2)
