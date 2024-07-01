1fab79423970fc11ff49ed900cf5404314a32742
Gonzalo Diaz <devel@gon.cl>
2024-07-01 19:36:19 -0400

    [REFACTOR] clean code: variable naming

--- src/hackerrank/interview_preparation_kit/arrays/new_year_chaos.py
+++ src/hackerrank/interview_preparation_kit/arrays/new_year_chaos.py
@@ -8,25 +8,25 @@ LOGGER = logging.getLogger(__name__)
 TOO_CHAOTIC_ERROR = 'Too chaotic'
 
 
-def minimum_bribes(q: list[int]) -> 'int | None':
+def minimum_bribes(queue: list[int]) -> 'int | None':
     bribes = 0
 
-    for i, o in enumerate(q):
-        cur = i + 1
+    for i, value in enumerate(queue):
+        position = i + 1
 
-        if o - cur > 2:
+        if value - position > 2:
             return None
 
-        for k in q[max(o - 2, 0):i]:
-            if k > o:
+        for k in queue[max(value - 2, 0):i]:
+            if k > value:
                 bribes += 1
 
     return bribes
 
 
-def minimum_bribes_transform(q: list[int]) -> 'int | str':
+def minimum_bribes_transform(queue: list[int]) -> 'int | str':
 
-    result = minimum_bribes(q)
+    result = minimum_bribes(queue)
     if result is None:
         return TOO_CHAOTIC_ERROR
 
