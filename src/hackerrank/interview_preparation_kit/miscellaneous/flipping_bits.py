dba91d7c4eb316ff2ed6fdc8663e63334aa0f4f0
Gonzalo Diaz <devel@gon.cl>
2024-07-01 20:03:33 -0400

    [REFACTOR] clean code: variable naming

--- src/hackerrank/interview_preparation_kit/miscellaneous/flipping_bits.py
+++ src/hackerrank/interview_preparation_kit/miscellaneous/flipping_bits.py
@@ -2,16 +2,16 @@
 # @link Problem definition [[docs/hackerrank/interview_preparation_kit/miscellaneous/flipping-bits.md]] # noqa
 # pylint: enable=line-too-long
 
-def flipping_bits(n: int) -> int:
+def flipping_bits(number: int) -> int:
     # print(n)
 
-    n_bin = bin(n)
+    n_bin = bin(number)
     n_bin_str = str(n_bin).replace("0b", '').rjust(32, "O")
     # print(f'{n_bin_str} lenght is {len(n_bin_str)}')
 
     result_bin_str = ''
-    for d in n_bin_str:
-        if d == '1':
+    for bin_digit in n_bin_str:
+        if bin_digit == '1':
             result_bin_str = result_bin_str + '0'
         else:
             result_bin_str = result_bin_str + '1'
