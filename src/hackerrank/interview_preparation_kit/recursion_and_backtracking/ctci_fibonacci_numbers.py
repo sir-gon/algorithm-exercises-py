5ac855f4b6aced087dbaf73e18c57370fab3d1ab
Gonzalo Diaz <devel@gon.cl>
2024-07-01 20:22:02 -0400

    [REFACTOR] clean code: variable naming

--- src/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_fibonacci_numbers.py
+++ src/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_fibonacci_numbers.py
@@ -2,10 +2,10 @@
 # @link Problem definition [[docs/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_fibonacci_numbers.md]] # noqa
 # pylint: enable=line-too-long
 
-def fibonacci(n):
-    if n == 0:
+def fibonacci(number: int):
+    if number == 0:
         return 0
-    if n == 1:
+    if number == 1:
         return 1
 
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci(number - 1) + fibonacci(number - 2)
