21eec98a764c08c42faf0dd29f64d5b943581d5a
Gonzalo Diaz <devel@gon.cl>
2024-07-29 17:46:02 -0400

    [Hacker Rank] Interview Preparation Kit: Dictionaries and Hashmaps: Frequency Queries. Basic test cases added.

--- src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/frequency_queries_test.py
+++ src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/frequency_queries_test.py
@@ -15,7 +15,49 @@ TEST_CASES = [
             [2, 5],
             [3, 2]
         ],
-        'answer': [0, 1]
+        'expected': [0, 1]
+    },
+    {
+        'title': 'Sample Test Case 2',
+        'input': [
+            [1, 3],
+            [2, 3],
+            [3, 2],
+            [1, 4],
+            [1, 5],
+            [1, 5],
+            [1, 4],
+            [3, 2],
+            [2, 4],
+            [3, 2]
+        ],
+        'expected': [0, 1, 1]
+    },
+    {
+        'title': 'Sample Test Case 3',
+        'input': [
+            [1, 3],
+            [1, 38],
+            [2, 1],
+            [1, 16],
+            [2, 1],
+            [2, 2],
+            [1, 64],
+            [1, 84],
+            [3, 1],
+            [1, 100],
+            [1, 10],
+            [2, 2],
+            [2, 1],
+            [1, 67],
+            [2, 2],
+            [3, 1],
+            [1, 99],
+            [1, 32],
+            [1, 58],
+            [3, 2]
+        ],
+        'expected': [1, 1, 0]
     }
 ]
 
@@ -27,9 +69,9 @@ class TestFreqQuery(unittest.TestCase):
         for _, _tt in enumerate(TEST_CASES):
 
             self.assertEqual(
-                freq_query(_tt['input']), _tt['answer'],
+                freq_query(_tt['input']), _tt['expected'],
                 f"{_} | freq_query({_tt['input']}) must be "
-                f"=> {_tt['answer']}")
+                f"=> {_tt['expected']}")
 
     def test_freq_query_edge_case(self):
 
