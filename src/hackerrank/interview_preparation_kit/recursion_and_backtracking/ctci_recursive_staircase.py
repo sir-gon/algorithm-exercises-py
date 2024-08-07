d4659ed4442ca3d8f9c6e4eec6b7e3fe9e1dd394
Gonzalo Diaz <devel@gon.cl>
2024-08-07 00:25:56 -0400

    [Hacker Rank] Interview Preparation Kit: Recursion: Davis' Staircase. Clean code improvements and new generalized solution.

--- src/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_recursive_staircase.py
+++ src/hackerrank/interview_preparation_kit/recursion_and_backtracking/ctci_recursive_staircase.py
@@ -4,23 +4,38 @@
 
 from typing import Dict
 
+TOP_LIMIT = 10 ** 10 + 7
+STEPS_LIMIT = 3
 
-def step_perms_comput_with_cache(n_steps: int, cache: Dict[int, int]) -> int:
+
+def step_perms_comput_with_cache(
+        n_steps: int,
+        cache: Dict[int, int],
+        steps_limit: int) -> int:
+
+    # Base cases
     if 0 <= n_steps <= 2:
         return n_steps
-    if n_steps == 3:
-        return 4
 
     result = 0
-    for i in range(1, 4):
-        if n_steps - i not in cache:
-            cache[n_steps - i] = step_perms_comput_with_cache(n_steps - i, cache)
+    for i in range(1, min(steps_limit, n_steps) + 1):
+        search_key: int = n_steps - i
+
+        if search_key not in cache:
+            cache[search_key] = step_perms_comput_with_cache(
+                search_key,
+                cache,
+                steps_limit
+            )
 
         result += cache[n_steps - i]
 
-    return result
+    # if n_steps <= steps_limit:
+    #     result += 1
+
+    return (result + 1) if n_steps <= steps_limit else result
 
 
 def step_perms(n_steps: int) -> int:
     initial_cache: Dict[int, int] = {}
-    return step_perms_comput_with_cache(n_steps, initial_cache) % (10 ** 10 + 7)
+    return step_perms_comput_with_cache(n_steps, initial_cache, STEPS_LIMIT) % TOP_LIMIT
