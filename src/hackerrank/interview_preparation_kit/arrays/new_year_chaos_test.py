5e48dcb6dcd5eeb96543b955afc2b25b723e99b1
Gonzalo Diaz <devel@gon.cl>
2024-08-13 17:16:31 -0400

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Arrays: New Year Chaos. JSON data load for unit tests.

--- src/hackerrank/interview_preparation_kit/arrays/new_year_chaos_test.py
+++ src/hackerrank/interview_preparation_kit/arrays/new_year_chaos_test.py
@@ -1,27 +1,22 @@
 import unittest
-from .new_year_chaos import minimum_bribes_transform, TOO_CHAOTIC_ERROR
+from pathlib import Path
+
+from ....hackerrank.lib.loader import load_test_cases
+from .new_year_chaos import minimum_bribes_transform
+
+FILE_PATH = str(Path(__file__).resolve().parent)
+
+TEST_CASES = load_test_cases(
+    FILE_PATH + '/new_year_chaos.testcases.json')
 
 
 class TestMinimumBribes(unittest.TestCase):
 
     def test_minimum_bribes(self):
 
-        tests = [
-            {'title': "Test Case 0-0",
-                'input': [2, 1, 5, 3, 4], 'answer': 3},
-            {'title': "Test Case 0-1",
-                'input': [2, 5, 1, 3, 4], 'answer': TOO_CHAOTIC_ERROR},
-            {'title': "Test Case 1-1",
-                'input': [5, 1, 2, 3, 7, 8, 6, 4], 'answer': TOO_CHAOTIC_ERROR},
-            {'title': "Test Case 1-2",
-                'input': [1, 2, 5, 3, 7, 8, 6, 4], 'answer': 7},
-            {'title': "Test Case 2",
-                'input': [1, 2, 5, 3, 4, 7, 8, 6], 'answer': 4},
-        ]
-
-        for _, _tt in enumerate(tests):
+        for _, _tt in enumerate(TEST_CASES):
 
             self.assertEqual(
-                minimum_bribes_transform(_tt['input']), _tt['answer'],
+                minimum_bribes_transform(_tt['input']), _tt['expected'],
                 f"{_} | minimum_bribes_transform({_tt['input']}) must be "
-                f"=> {_tt['answer']}")
+                f"=> {_tt['expected']}")
