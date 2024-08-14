0b288ad3fbeac7ead3e5e6755d746a7912f756fd
Gonzalo Diaz <devel@gon.cl>
2024-08-13 22:44:30 -0400

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Strings: Making Anagrams. JSON data load for unit tests.

--- src/hackerrank/interview_preparation_kit/string_manipulation/ctci_making_anagrams_test.py
+++ src/hackerrank/interview_preparation_kit/string_manipulation/ctci_making_anagrams_test.py
@@ -1,26 +1,12 @@
 import unittest
+from pathlib import Path
+
+from ....hackerrank.lib.loader import load_test_cases
 from .ctci_making_anagrams import make_anagram
 
-TEST_CASES = [
-    {
-        'title': 'Sample Test case 0',
-        'a': 'cde',
-        'b': 'abc',
-        'answer': 4
-    },
-    {
-        'title': 'Sample Test case 1',
-        'a': 'fcrxzwscanmligyxyvym',
-        'b': 'jxwtrhvujlmrpdoqbisbwhmgpmeoke',
-        'answer': 30
-    },
-    {
-        'title': 'Sample Test case 2',
-        'a': 'showman',
-        'b': 'woman',
-        'answer': 2
-    }
-]
+FILE_PATH = str(Path(__file__).resolve().parent)
+
+TEST_CASES = load_test_cases(FILE_PATH + '/ctci_making_anagrams.testcases.json')
 
 
 class TestMakeAnagram(unittest.TestCase):
@@ -30,6 +16,6 @@ class TestMakeAnagram(unittest.TestCase):
         for _, _tt in enumerate(TEST_CASES):
 
             self.assertEqual(
-                make_anagram(_tt['a'], _tt['b']), _tt['answer'],
+                make_anagram(_tt['a'], _tt['b']), _tt['expected'],
                 f"{_} | make_anagram({_tt['a']}, {_tt['b']}) must be "
-                f"=> {_tt['answer']}")
+                f"=> {_tt['expected']}")
