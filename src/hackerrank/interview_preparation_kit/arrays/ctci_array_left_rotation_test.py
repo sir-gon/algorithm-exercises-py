2bf1d8f879d9264983f74f765be7036c8744c594
Gonzalo Diaz <devel@gon.cl>
2025-02-20 23:56:08 -0300

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Arrays: Left Rotation. New JSON data.

--- src/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation_test.py
+++ src/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation_test.py
@@ -2,7 +2,7 @@ import unittest
 from pathlib import Path
 
 from ....lib.loader import loadTestCases
-from .ctci_array_left_rotation import rotLeft, rotLeftOne
+from .ctci_array_left_rotation import rotLeft
 
 FILE_PATH = str(Path(__file__).resolve().parent)
 
@@ -12,23 +12,11 @@ TEST_CASES = loadTestCases(
 
 class TestArrayLeftRotation(unittest.TestCase):
 
-    def test_rot_left_one(self):
+    def test_rot_left(self):
+
         for _, _tt in enumerate(TEST_CASES):
 
             self.assertEqual(
-                rotLeftOne(_tt['input']), _tt['expected'],
-                f"{_} | rotLeftOne({_tt['input']}) must be "
-                f"=> {_tt['expected']}")
-
-    def test_rot_left(self):
-
-        tests = [
-            {'input': [1, 2, 3, 4, 5], 'd': 4, 'expected': [5, 1, 2, 3, 4]},
-        ]
-
-        for _, _tt in enumerate(tests):
-
-            self.assertEqual(
-                rotLeft(_tt['input'], _tt['d']), _tt['expected'],
-                f"{_} | rotLeft({_tt['input']}) must be "
+                rotLeft(_tt['input'], _tt['d_rotations']), _tt['expected'],
+                f"{_} | rotLeft({_tt['input'], _tt['d_rotations']}) must be "
                 f"=> {_tt['expected']}")
